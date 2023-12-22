f46e242783568f3c639b98a18b7fb909d922b685
taiprogrammer <taiza.marques@sptech.school>
2023-12-22 17:07:16 -0300

    feature: adding plop configuration

---
+++ generators/plopfile.js
@@ -0,0 +1,35 @@
+export default plop => {
+	// create your generators here
+	plop.setGenerator("component", {
+		description: "Create a component",
+		prompts: [
+			{
+				type: "input",
+				name: "name",
+				message: "What is your component name?",
+			},
+		],
+		actions: [
+			{
+				type: "add",
+				path: "../src/components/{{pascalCase name}}/index.tsx",
+				templateFile: "templates/Component.tsx.hbs",
+			},
+			{
+				type: "add",
+				path: "../src/components/{{pascalCase name}}/styles.scss",
+				templateFile: "templates/styles.scss.hbs",
+			},
+			{
+				type: "add",
+				path: "../src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx",
+				templateFile: "templates/Component.stories.tsx.hbs",
+			},
+			{
+				type: "add",
+				path: "../src/components/{{pascalCase name}}/{{pascalCase name}}.spec.tsx",
+				templateFile: "templates/Component.spec.tsx.hbs",
+			},
+		],
+	});
+};
