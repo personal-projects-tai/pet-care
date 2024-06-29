e2f9ce04a1c7384419c89fd07966658d3d5ededc
taiprogrammer <taizamarquesousa@gmail.com>
2024-06-28 23:35:06 -0300

    feature: adding tests StepsContext

---
+++ src/hooks/StepsContext.spec.tsx
@@ -0,0 +1,88 @@
+import { router } from '@/router'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { StepsProvider, useSteps } from './StepsContext'
+import { STEPS_PATHS } from './@types/steps'
+import { render, screen } from '@testing-library/react'
+import { act } from 'react-dom/test-utils'
+
+vi.mock('@/router', () => ({
+  router: {
+    state: {
+      location: {
+        pathname: ''
+      }
+    },
+    navigate: vi.fn()
+  }
+}))
+
+const TestComponent = () => {
+  const { step, actionStep, numericStep } = useSteps()
+
+  return (
+    <div>
+      <div data-testid="step">{step}</div>
+      <div data-testid="numericStep">{numericStep}</div>
+      <button onClick={() => actionStep(STEPS_PATHS.NAME)}>Go to name</button>
+    </div>
+  )
+}
+
+describe('StepsProvider', () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+  })
+
+  it('initial state is breed step and numericStep is 1', () => {
+    render(
+      <StepsProvider>
+        <TestComponent />
+      </StepsProvider>
+    )
+
+    expect(screen.getByTestId('step')).toHaveTextContent('breed')
+    expect(screen.getByTestId('numericStep')).toHaveTextContent('1')
+  })
+
+  it('actionStep updates step and numericStep', () => {
+    render(
+      <StepsProvider>
+        <TestComponent />
+      </StepsProvider>
+    )
+
+    act(() => {
+      screen.getByText('Go to name').click()
+    })
+
+    expect(router.navigate).toHaveBeenCalledWith(STEPS_PATHS.NAME)
+    expect(screen.getByTestId('step')).toHaveTextContent('name')
+    expect(screen.getByTestId('numericStep')).toHaveTextContent('2')
+  })
+
+  it('toggleStep updates step and numericStep based on currentStep', () => {
+    router.state.location.pathname = STEPS_PATHS.NAME
+
+    render(
+      <StepsProvider>
+        <TestComponent />
+      </StepsProvider>
+    )
+
+    expect(screen.getByTestId('step')).toHaveTextContent('name')
+    expect(screen.getByTestId('numericStep')).toHaveTextContent('2')
+  })
+
+  it('toggleStep handles and unknown path', () => {
+    router.state.location.pathname = '/unknown'
+
+    render(
+      <StepsProvider>
+        <TestComponent />
+      </StepsProvider>
+    )
+
+    expect(screen.getByTestId('step')).toHaveTextContent('breed')
+    expect(screen.getByTestId('numericStep')).toHaveTextContent('1')
+  })
+})
