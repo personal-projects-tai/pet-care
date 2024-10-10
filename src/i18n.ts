f9af5253100d70807683e23465483a2df26ccd7c
taiprogrammer <taizamarquesousa@gmail.com>
2024-10-09 22:04:47 -0300

    feature: adding japanese, italian, chinese and ukranian languages

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,8 +4,12 @@ import { initReactI18next } from 'react-i18next'
 import englishTranslation from './locales/en/translation.json'
 import spanishTranslation from './locales/es/translation.json'
 import frenchTranslation from './locales/fr/translation.json'
+import italianTranslation from './locales/it-IT/translation.json'
+import japaneseTranslation from './locales/ja-JP/translation.json'
 import brazilianTranslation from './locales/pt-BR/translation.json'
 import portugueseTranslation from './locales/pt-PT/translation.json'
+import ukranianTranslation from './locales/uk-UA/translation.json'
+import chineseTranslation from './locales/zh-CN/translation.json'
 
 const resources = {
   en: {
@@ -22,6 +26,18 @@ const resources = {
   },
   fr: {
     translation: frenchTranslation
+  },
+  'ja-JP': {
+    translation: japaneseTranslation
+  },
+  'it-IT': {
+    translation: italianTranslation
+  },
+  'zh-CN': {
+    translation: chineseTranslation
+  },
+  'uk-UA': {
+    translation: ukranianTranslation
   }
 }
 
